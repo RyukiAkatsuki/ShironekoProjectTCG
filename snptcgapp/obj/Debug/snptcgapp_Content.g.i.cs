5b8d3f73ceecfd2880ca863cf0640628a184ca2b
R_K <shawnfroste@outlook.fr>
2024-12-28 15:05:06 +0100

    Adding Menu with some images

--- snptcgapp/obj/Debug/snptcgapp_Content.g.i.cs
+++ snptcgapp/obj/Debug/snptcgapp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons8-home-50.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons8-info-50.png")]
 
 
